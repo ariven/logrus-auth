06ecbe76492a8093f385e2e30f413a2e44afa0ef
Patrick Spence <ariven@gmail.com>
2012-05-18 09:47:30 -0700

    added mail template

---
+++ application/views/mail/template.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+</head>
+<body>
+<h3><?php echo site_url();?></h3>
+<hr />
+<?php echo $content; ?>
+</body>
+</html>
