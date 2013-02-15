3d5023ae163ff28ce4af776d7824cc9853e765e4
vitamist <pspence@vitamist.com>
2013-02-15 14:10:43 -0700

    Update logrus_notify.php config
    Updated config to redact my email addresses.. sigh, long day.

--- application/config/logrus_notify.php
+++ application/config/logrus_notify.php
@@ -5,8 +5,8 @@
  * Time: 9:34 AM
  *
  */
-$config['logrus_mail_from']   = 'Administrator <gemini@ariven.com>'; // who we send mail from
-$config['logrus_reply_to']    = 'Reply to me <gemini@ariven.com>'; // who to reply to
-$config['logrus_admin_email'] = 'pspence@vitamist.com'; // administrators email, used in logrus_notify->admin()
+$config['logrus_mail_from']   = 'Administrator <administrator@example.com>'; // who we send mail from
+$config['logrus_reply_to']    = 'Reply to me <administrator@example.com>'; // who to reply to
+$config['logrus_admin_email'] = 'administrator@example'; // administrators email, used in logrus_notify->admin()
 $config['logrus_admin_name']  = 'Administrator'; // used in logrus_notify->admin()
 $config['logrus_site_name']   = 'Generic Site'; // used in logrus_notify->admin()
